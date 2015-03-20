c76a66cebb1289f96d9185be94151b23da2535d7
StopSign <jimkap09@gmail.com>
2015-03-19 23:04:54 -0400

    GA

--- IncrementalScript.js
+++ IncrementalScript.js
@@ -1,6 +1,8 @@
 
-/*
-setInterval(function() {
+
+
+
+/*setInterval(function() {
 	tick();
 },50);*/
 
@@ -12,6 +14,14 @@ doWork.onmessage = function(event) {
     }
 };
 doWork.postMessage({start:true,ms:50});
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-60978932-1', 'auto');
+ga('send', 'pageview');
 
 loadDefaults();
 loadFromStorage();
