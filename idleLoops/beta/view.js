48356362550606e93eeba1d7af35a2ae97176f74
Antoine Bertier <antoine.bertier@safti.fr>
2018-06-23 00:00:38 +0200

    Changing Talent to Bonus XP % in the radar graph, custum text for the tooltip

--- idleLoops/beta/view.js
+++ idleLoops/beta/view.js
@@ -34,6 +34,20 @@ function View() {
                   tension:0,
                   borderWidth:3
                 }
+              },
+              tooltips: {
+                  callbacks: {
+                      label: function(tooltipItem, data) {
+                          var label = data.datasets[tooltipItem.datasetIndex].label || '';
+
+                          if (label) {
+                              label += ': ';
+                          }
+                          label += Math.round(tooltipItem.yLabel * 100) / 100;
+                          label += data.datasets[tooltipItem.datasetIndex].tooltipComplement || '';
+                          return label;
+                      }
+                  }
               }
             },
             data: {
@@ -88,19 +102,20 @@ function View() {
           pointHoverBorderColor:"rgb(255, 99, 132)"
         },
         {
-          label : "Talent",
+          label : "Bonus XP",
           data : [],
           backgroundColor:"rgba(54, 162, 235, 0.2)",
           borderColor:"rgb(54, 162, 235)",
           pointBackgroundColor:"rgb(54, 162, 235)",
           pointBorderColor:"#fff",
           pointHoverBackgroundColor:"#fff",
-          pointHoverBorderColor:"rgb(54, 162, 235)"
+          pointHoverBorderColor:"rgb(54, 162, 235)",
+          tooltipComplement : "%",
         }
       ];
       for(let i = 0; i < statList.length; i++) {
           dataset[0].data.push(getLevel(statList[i]));
-          dataset[1].data.push(getTalent(statList[i]));
+          dataset[1].data.push(getTotalBonusXP(statList[i],2));
       };
       return dataset;
     }
