46b18edf8e1905f1bd0222d67a706f1bd22db12b
James Kaplan <jimkap09@gmail.com>
2018-06-25 21:08:34 -0400

    Idle Loops - removing bonus mana

--- idleLoops/beta/view.js
+++ idleLoops/beta/view.js
@@ -24,7 +24,7 @@ function View() {
     };
 
     this.statBlurbs = ["Know your body.", "Train your body.", "Just a little longer. Just a little more.", "Gotta go fast.", "Look a little closer...", "Conversation is a battle.", "Learning to learn.", "Opportunity favors the fortunate.", "You are the captain."];
-    this.statLocs = [{x:165, y:43}, {x:272, y:79}, {x:325, y:170}, {x:305, y:284}, {x:223, y:352}, {x:102, y:352}, {x:24, y:284}, {x:2, y:170}, {x:56, y:79}];
+    this.statLocs = [{x:165, y:43}, {x:270, y:79}, {x:325, y:170}, {x:306, y:284}, {x:225, y:352}, {x:102, y:352}, {x:26, y:284}, {x:2, y:170}, {x:56, y:79}];
     this.createStats = function() {
         statGraph.init();
         let statContainer = document.getElementById("statContainer");
